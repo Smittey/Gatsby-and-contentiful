a23729c464c910114722d1df63601ea2cf76412c
smittey <andy_smithy@hotmail.co.uk>
2019-11-21 19:08:58 +0000

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import Hero from '../components/hero'
+import Layout from '../components/layout'
+import ArticlePreview from '../components/article-preview'
+
+class RootIndex extends React.Component {
+  render() {
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const profileHeader = get(this, 'props.data.contentfulProfileHeader')
+    // const posts = get(this, 'props.data.allContentfulBlogPost.edges')
+
+    return (
+      <Layout location={this.props.location} >
+        <div style={{ background: '#fff' }}>
+          <Helmet title={siteTitle} />
+
+          <div className="wrapper">
+            <h4>{profileHeader.specialism}</h4>
+            <h4>{profileHeader.location}</h4>
+            <div>{profileHeader.name}</div>            
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default RootIndex
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
+      edges {
+        node {
+          title
+          slug
+          publishDate(formatString: "MMMM Do, YYYY")
+          tags
+          heroImage {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+             ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+          description {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+      }
+    }
+    allContentfulPerson(filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }) {
+      edges {
+        node {
+          name
+          shortBio {
+            shortBio
+          }
+          title
+          heroImage: image {
+            fluid(
+              maxWidth: 1180
+              maxHeight: 480
+              resizingBehavior: PAD
+              background: "rgb:000000"
+            ) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
+        }
+      }
+    }
+    contentfulProfileHeader {
+      specialism
+      name
+      location
+    }
+  }
+`
